9154280b4b9ee0b1189ef6c205b97767df11df90
MD Shoaib Khan <abrohoman019@gmail.com>
2025-06-17 13:42:57 +0600

    Refactor RE_ab2plus.c to improve code structure by moving variable declarations inside the loop

--- Compiler Design/RE_ab2plus.c
+++ Compiler Design/RE_ab2plus.c
@@ -2,22 +2,25 @@
 
 int main() {
     char str[256];
-    int i = 0, count = 0;
-    while(1){
 
-    printf("Enter : ");
-    fgets(str, sizeof(str), stdin); 
+    while (1) {
+        int i = 0, count = 0;
 
-    while (str[i] == 'a' && str[i + 1] == 'b') {
-        count++;
-        i += 2;
-    }
+        printf("Enter : ");
+        fgets(str, sizeof(str), stdin);
 
-    if ((str[i] == '\n' || str[i] == '\0') && count >= 1) {
-        printf("Accepted: matches (ab)+\n");
-    } else {
-        printf("Rejected: does not match (ab)+\n");
-    }
+      
+        while (str[i] == 'a' && str[i + 1] == 'b') {
+            count++;
+            i += 2;
+        }
+
+   
+        if ((str[i] == '\n' || str[i] == '\0') && count >= 1) {
+            printf("Accepted: matches (ab)+\n");
+        } else {
+            printf("Rejected: does not match (ab)+\n");
+        }
     }
 
     return 0;
