4668fc7842d7158e723b222ce564147e93882096
MD Shoaib Khan <abrohoman019@gmail.com>
2025-06-17 12:46:11 +0600

    Refactor whitespace counting to use getchar for input handling

--- Compiler Design/count_whitespace_in_line.c
+++ Compiler Design/count_whitespace_in_line.c
@@ -3,24 +3,28 @@
 int main() {
     char line[256];
     int space_count = 0, tab_count = 0, newline_count = 0, vtab_count = 0, ffeed_count = 0, cr_count = 0;
+    int i = 0, c;
     printf("Enter a line of text:\n");
-    if (fgets(line, sizeof(line), stdin)) {
-        for (int i = 0; line[i] != '\0'; i++) {
-            if (line[i] == ' ') space_count++;
-            else if (line[i] == '\t') tab_count++;
-            else if (line[i] == '\n') newline_count++;
-            else if (line[i] == '\v') vtab_count++;
-            else if (line[i] == '\f') ffeed_count++;
-            else if (line[i] == '\r') cr_count++;
+    while (i < 255) {
+        c = getchar();
+        if (c == EOF || c == '\n') {
+            line[i] = '\0';
+            if (c == '\n') newline_count++;
+            break;
         }
-        printf("Spaces: %d\n", space_count);
-        printf("Tabs: %d\n", tab_count);
-        printf("Newlines: %d\n", newline_count);
-        printf("Vertical Tabs: %d\n", vtab_count);
-        printf("Form Feeds: %d\n", ffeed_count);
-        printf("Carriage Returns: %d\n", cr_count);
-    } else {
-        printf("Error reading input.\n");
+        line[i] = (char)c;
+        if (c == ' ') space_count++;
+        else if (c == '\t') tab_count++;
+        else if (c == '\v') vtab_count++;
+        else if (c == '\f') ffeed_count++;
+        else if (c == '\r') cr_count++;
+        i++;
     }
+    printf("Spaces: %d\n", space_count);
+    printf("Tabs: %d\n", tab_count);
+    printf("Newlines: %d\n", newline_count);
+    printf("Vertical Tabs: %d\n", vtab_count);
+    printf("Form Feeds: %d\n", ffeed_count);
+    printf("Carriage Returns: %d\n", cr_count);
     return 0;
 }
