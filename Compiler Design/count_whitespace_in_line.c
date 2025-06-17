64eeda3861419b6f59b8dfd37e299549f7e2dc97
MD Shoaib Khan <abrohoman019@gmail.com>
2025-06-17 12:45:16 +0600

    Refactor whitespace counting logic to provide detailed counts for each type of whitespace character

--- Compiler Design/count_whitespace_in_line.c
+++ Compiler Design/count_whitespace_in_line.c
@@ -2,15 +2,23 @@
 
 int main() {
     char line[256];
-    int count = 0;
+    int space_count = 0, tab_count = 0, newline_count = 0, vtab_count = 0, ffeed_count = 0, cr_count = 0;
     printf("Enter a line of text:\n");
     if (fgets(line, sizeof(line), stdin)) {
         for (int i = 0; line[i] != '\0'; i++) {
-            if (line[i] == ' ' || line[i] == '\t' || line[i] == '\n' || line[i] == '\v' || line[i] == '\f' || line[i] == '\r') {
-                count++;
-            }
+            if (line[i] == ' ') space_count++;
+            else if (line[i] == '\t') tab_count++;
+            else if (line[i] == '\n') newline_count++;
+            else if (line[i] == '\v') vtab_count++;
+            else if (line[i] == '\f') ffeed_count++;
+            else if (line[i] == '\r') cr_count++;
         }
-        printf("Number of whitespace characters: %d\n", count);
+        printf("Spaces: %d\n", space_count);
+        printf("Tabs: %d\n", tab_count);
+        printf("Newlines: %d\n", newline_count);
+        printf("Vertical Tabs: %d\n", vtab_count);
+        printf("Form Feeds: %d\n", ffeed_count);
+        printf("Carriage Returns: %d\n", cr_count);
     } else {
         printf("Error reading input.\n");
     }
